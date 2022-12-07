6da6ab35bb6653d2a30d7ff47baf9da38ab3c045
trizcard <beatrizco@usp.br>
2022-12-07 16:55:25 -0300

    att

--- Trabalho3/headers/funcPrincipais.h
+++ Trabalho3/headers/funcPrincipais.h
@@ -1,5 +1,7 @@
 #include "funcAuxiliares.h"
 
-
-void lerGrafo(FILE *arq, grafo *g);
-int caminhoMinimo(int orig, int fim, grafo *g);
+void lerCabecalho(FILE *arq, cabecalho *cab);
+void alocaRegistro(registro *reg);
+void desalocarRegistro(registro *reg);
+int lerRegistro(FILE *arq, registro *reg);
+void lerCampoVar(char *string, char *strPoPs, char *strPais);
